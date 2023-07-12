58db88626e42c36212ea81e1962d4ade9289070e
Nikitha1402 <119092964+Nikitha1402@users.noreply.github.com>
2023-07-12 16:21:43 +0530

    Add files via upload

---
+++ Day4/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from '../navbar/Navbar';
+import './Home.css';
+function Home() {
+  return (
+    <div className='app3'>
+    <Navbar />
+    <br></br><br></br><br></br><br></br>
+    <br></br>    <br></br><br></br><br></br><br></br>
+    <br></br><br></br><br></br><br></br>
+    <br></br><br></br>
+
+
+    <h2>The Best University <br></br>Of the State</h2>
+    
+    </div>
+  );
+}
+
+export default Home;
